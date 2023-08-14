09ca107c7936e725f11c5d2132ae563c97217445
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2023-08-14 21:15:20 +0900

    다시 sfnt reader 복귀했는데, unpack이 필요함

--- packages/sfnt/src/tt_lib/sfnt.rs
+++ packages/sfnt/src/tt_lib/sfnt.rs
@@ -1,4 +1,5 @@
-use std::io::{Read, Seek};
+use crate::misc::sstruct::get_sfnt_directory_size;
+use std::io::{Error, ErrorKind, Read, Seek};
 pub struct SFNTReader {}
 
 impl SFNTReader {
@@ -9,6 +10,24 @@ impl SFNTReader {
         // can be b"wOFF", b"ttcf", or b"OTTO"
         println!("is sfnt_version b\"OTTO\": {:?}", &bytes == b"OTTO");
         file.seek(std::io::SeekFrom::Start(0))?;
+
+        match &bytes {
+            b"ttcf" => {
+                unimplemented!("SFNT Reader for True Type Collection not implemented")
+            }
+            b"wOFF" => {
+                unimplemented!("SFNT Reader for WOFF Webfont not implemented")
+            }
+            _ => {
+                let sfnt_directory_size = get_sfnt_directory_size();
+                let mut buf = vec![0u8; sfnt_directory_size];
+                let Ok(_) = file.read_exact(&mut buf) else {
+                    return Err(Error::new(ErrorKind::Other, "Not a TrueType or OpenType font (not enough data)"));
+                };
+
+                // open type or true type
+            }
+        }
 
         println!("SFNTReader.new(input_path: &str) not yet implemented");
         Ok(Self {})
